958f52363ea8d9b3a551678342bf09516f5a813e
Gabriel Figueroa <gabriel.figueroa2020@gmail.com>
2021-05-04 15:29:58 -0400

    Add files via upload

---
+++ CaptureReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Azure.Storage.Blobs;
+
+namespace CaptureReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void StartProcessing()
+        {
+            Console.WriteLine($"Processor started using path: {Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}");
+            var container = BlobContainerClient.WebBlobContainerName;
+        }
+    }
+}
